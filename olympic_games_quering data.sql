d8272009146a46df91f9784073efd4dc4a256b85
Shreyas <87318121+kdshreyas@users.noreply.github.com>
2021-10-21 17:03:41 +0530

    Add files via upload

---
+++ olympic_games_quering data.sql
@@ -0,0 +1,26 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (100) *
+  FROM  [olympic_games].[dbo].[athletes_event_results]
+
+  SELECT ID
+		,Name as 'Competitor Name' -- Renamed Column
+		,CASE WHEN SEX = 'M' THEN 'Male'
+		 ELSE 'Female'
+		 END AS Sex -- Better name for filters and visualisations
+		 ,Age
+
+			,CASE WHEN Age < 18 THEN 'Under 18'
+				WHEN Age BETWEEN 18 AND 25 THEN '18-25'
+				WHEN Age BETWEEN 25 AND 30 THEN '25-30'
+				WHEN Age > 30 THEN 'Over 30'
+				END AS'Age Grouping'
+		 ,Height
+		 ,Weight
+		 ,NOC AS 'Nation Code'  -- Expanded abbreviation
+		 ,LEFT(Games, CHARINDEX(' ', Games) - 1) AS 'Year' -- Split column to isolate Year, based on space
+		,CASE WHEN Medal = 'NA' THEN 'Not Registered' 
+		ELSE Medal
+		END AS Medal -- Replaced NA with Not Registered
+
+		 from  [olympic_games].[dbo].[athletes_event_results]
+		  WHERE RIGHT(Games,CHARINDEX(' ', REVERSE(Games))-1) = 'Summer' -- Where Clause to isolate Summer Season
